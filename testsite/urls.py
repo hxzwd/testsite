95038d001a4ee2560d840dc53ed470794888f6c2
hxzwd <omnomnomrus43@gmail.com>
2015-04-29 23:08:43 +0300

    nachalo polojeno

---
+++ testsite/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'testsite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    #url(r'^admin/', include(admin.site.urls)),
+	url(r'^hello/$', views.hello, name='hello'),
+]
